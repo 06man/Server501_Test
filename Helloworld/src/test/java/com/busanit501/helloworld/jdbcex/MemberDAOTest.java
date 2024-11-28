b7aa890dab9038f42c23f6f038ee153578ebc99b
이상용 <lsy3709@naver.com>
2024-11-28 10:19:57 +0900

    2교시, MemberDAO 추가, mid,mpw 회원 조회하기, 단위 테스트

---
+++ Helloworld/src/test/java/com/busanit501/helloworld/jdbcex/MemberDAOTest.java
@@ -0,0 +1,26 @@
+package com.busanit501.helloworld.jdbcex;
+
+import com.busanit501.helloworld.jdbcex.dao.MemberDAO;
+import com.busanit501.helloworld.jdbcex.vo.MemberVO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+
+public class MemberDAOTest {
+    private MemberDAO memberDAO;
+
+    // 아래에 각 단위 테스트가 실행되기전에, 먼저 실행을 함.
+    @BeforeEach
+    public void ready() {
+        memberDAO = new MemberDAO();
+    }
+
+    @Test
+    public void getMemberWithMpwTest() throws SQLException {
+        String mid = "lsy";
+        String mpw = "1234";
+       MemberVO memberVO = memberDAO.getMemberWithMpw(mid,mpw);
+    }
+
+}
