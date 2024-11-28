ed37d654532e2129f5e422f18fe0f775bacf97ab
이상용 <lsy3709@naver.com>
2024-11-28 14:34:55 +0900

    5교시, 쿠키 찾는 프로세스, 조회한 todo 번호를 쿠키에 저장하고, 웹 브라우저에게 전달 할 예정.3 findCookie 이용해서, 쿠키의 내용 유무에 따라서, 없으면 내용 추가, 있으면 계속 사용

--- Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoRead2Controller.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoRead2Controller.java
@@ -2,6 +2,7 @@ package com.busanit501.helloworld.jdbcex.controller;
 
 import com.busanit501.helloworld.jdbcex.dto.TodoDTO;
 import com.busanit501.helloworld.jdbcex.service.TodoService;
+import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.SQLException;
-
+@Log4j2
 @WebServlet(name = "TodoRead2Controller", urlPatterns = "/todo/read2")
 public class TodoRead2Controller extends HttpServlet {
     private TodoService todoService = TodoService.INSTANCE;
@@ -38,7 +39,27 @@ public class TodoRead2Controller extends HttpServlet {
 
             //1) 쿠키 조회하기. - 전체 쿠키 내용중에서, 내가 찾는 쿠키가 있니?
 //            findCookie(전체 쿠키 배열, 찾고자하는 쿠키 이름) -> 반환 해당 쿠키,
-
+           Cookie findCookie = findCookie(request.getCookies(), "viewTodos");
+           // 쿠키의 값을 조회하기.
+           String cookieValue = findCookie.getValue();
+           log.info("cookieValue : " + cookieValue);
+           // 상태 변수,
+            // 조회한 게시글 번호 ->예시) cookieValue =  "1-3-5-7-"
+            boolean exists = false;
+            // 기본 유효성 체크, 쿠키의 존재 및, 내용의 존재여부 확인
+            if(cookieValue != null && cookieValue.indexOf(tno+"-") >=0) {
+                log.info("cookieValue.indexOf(tno+\"-\") " + cookieValue.indexOf(tno+"-"));
+                exists = true;
+            }
+            // 만약에, 쿠키의 내용이 없다면, 내용을 추가하기.
+            if(!exists) {
+                cookieValue += tno+"-";
+                findCookie.setValue(cookieValue);
+                findCookie.setMaxAge(60*60*24);
+                findCookie.setPath("/");
+                // 무조건 서버에서 응답 해주기.
+                response.addCookie(findCookie);
+            }
 
             request.getRequestDispatcher("/WEB-INF/todo/todoRead2.jsp")
                     .forward(request, response);
