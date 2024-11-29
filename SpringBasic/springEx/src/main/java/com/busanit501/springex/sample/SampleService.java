cb4e4015ee3aa345ea23e17ff85210560f449202
이상용 <lsy3709@naver.com>
2024-11-29 16:16:45 +0900

    7교시, 스프링 기초,의존성 주입 작업 낱개로 등록하는 방법, 통으로 패키지를 등록해서, 이용하는 방법 <context:component-scan>, 단위테스트 , 각역할, @Service, @Repository 확인

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
@@ -3,11 +3,12 @@ package com.busanit501.springex.sample;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 // 컨트롤러 역할 , @Controller
 // 서비스 역할 , @Service
 // DAO 역할, @Repository
-@Component // 일반 객체 타입을 시스템 등록,
+@Service // 일반 객체 타입을 시스템 등록,
 @ToString
 public class SampleService {
     @Autowired
