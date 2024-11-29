dd2cd04b4edc2618059063ba3ec6022541d171af
이상용 <lsy3709@naver.com>
2024-11-29 14:22:24 +0900

    5교시, 리스너 클래스이용해서, 어느파일에서든 접근이 가능한 공간에 임의의 변수를 설정하고, 가져오는 예1

--- Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoList2Controller.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/controller/TodoList2Controller.java
@@ -5,6 +5,7 @@ import com.busanit501.helloworld.jdbcex.dto.TodoDTO;
 
 import lombok.extern.log4j.Log4j2;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -24,6 +25,12 @@ public class TodoList2Controller extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // TestListener , 에 등록된 특정 변수에 접근,
+        // 그러면, 전역, 공유 자원 처럼 사용이 되어서,
+        // 어느 파일에서도 접근이 가능함,
+        ServletContext context = request.getServletContext();
+        String result = (String) context.getAttribute("appTestName");
+        log.info("TodoList2Controller ServletContext : 값 조회 확인중 : "+result);
         //
         log.info("doGet TodoList2Controller 확인");
         try {
--- Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/listener/TestListener.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/listener/TestListener.java
@@ -2,6 +2,7 @@ package com.busanit501.helloworld.jdbcex.listener;
 
 import lombok.extern.log4j.Log4j2;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -12,6 +13,13 @@ public class TestListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         log.info("contextInitialized :초기화 중 init");
+
+        //
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("appTestName","helloworld");
+
+
+
     }
 
     @Override
