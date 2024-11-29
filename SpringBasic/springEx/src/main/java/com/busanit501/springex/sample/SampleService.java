0800343f3fb4c3e897607df656c73cbf6de0b236
이상용 <lsy3709@naver.com>
2024-11-29 15:39:45 +0900

    6교시, 스프링 기초,의존성 주입 작업 준비3, SampleService에 SampleDAO 주입해보기

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/sample/SampleService.java
@@ -1,4 +1,11 @@
 package com.busanit501.springex.sample;
 
+import lombok.ToString;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ToString
 public class SampleService {
+//    @Autowired
+//    private SampleDAO sampleDAO;
+
 }
