00b866e131240e31110b0aa49d7614d3b7a9617b
이상용 <lsy3709@naver.com>
2024-11-29 09:50:07 +0900

    1교시, 자동로그인 쿠키 사용해서 , 로그인 필터 부분에서 구현 작업중

--- Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/filter/LoginFilter.java
+++ Helloworld/src/main/java/com/busanit501/helloworld/jdbcex/filter/LoginFilter.java
@@ -1,14 +1,17 @@
 package com.busanit501.helloworld.jdbcex.filter;
 
 import com.busanit501.helloworld.jdbcex.dto.MemberDTO;
+import com.busanit501.helloworld.jdbcex.service.MemberService;
 import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.sql.SQLException;
 
 //톰캣 서버에서,
 // 한글로 입력된 내용을, UTF8로 변환해서 보내기.
@@ -37,12 +40,40 @@ public class LoginFilter implements Filter {
         }
         // 2번째 이후의 방문, 하지만, 세션이라는 저장공간,
         // 여기에 키 : loginInfo , 값: 로그인한 유저의 아이디를 기록.
-        if(session.getAttribute("loginInfo") == null) {
+        if(session.getAttribute("loginInfo") != null) {
             log.info("2번째 이후로 서버에 요청을했고, 하지만, 로그인 정보는 없는 경우.");
             // 로그인 컨트롤러가 아직 없음, 곧 만들 예정.
+//            response.sendRedirect("/login");
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        // 자동로그인, 구조
+        // Page 범위, -> HttpServletRequest -> HttpSestion -> Application
+        // HttpServletRequest 에서, 모든 쿠키를 검색해서, rememberMe  이름 찾기
+        // 만약, 해당 쿠키 가 있고, 값도 있다면,
+        // 디비에 조회를 해서, 쿠키의 값, 디비의 값 일치성 확인?
+        // loginInfo MemberDTO의 값을 설정.
+
+        // 내일 로그인 필터에서 처리하는 부분 보기.
+        //findCookie, 해당 유저의 uuid  있음.
+        Cookie findCookie = findCookie(request.getCookies(), "rememberMe");
+        // rememberMe 가 체크가 안되서, null 경우, 다시 로그인 직접하기.
+        if(findCookie == null) {
             response.sendRedirect("/login");
             return;
         }
+       String getUuid = findCookie.getValue();
+
+        try {
+            MemberDTO memberDTO = MemberService.INSTANCE.getMemberWithUuidService(getUuid);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+//            response.sendRedirect("/login");
+
+        }
+
+
         //임시로, 최초도 아니고, 로그인 처리가 되었다면, 그러면,
         // 정상적으로 접근하는 페이지로 이동 시켜 줄게.
         if(session.getAttribute("loginInfo") != null) {
@@ -53,4 +84,20 @@ public class LoginFilter implements Filter {
         }
         filterChain.doFilter(servletRequest, servletResponse);
     }
+
+    private Cookie findCookie(Cookie[] cookies, String name) {
+        Cookie findCookie = null;
+        // 쿠키가 있는 경우
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                // cookie.getName(): 전체 쿠키 목록 요소의 이름
+                // name : 찾고자하는 쿠키 이름.
+                if (cookie.getName().equals(name)) {
+                    findCookie = cookie;
+                    break;
+                } //if
+            } // for
+        } // if
+        return findCookie;
+    } // method
 }
