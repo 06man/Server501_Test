c0992c09e7a2bca58da9d6b41aa024c202eba35e
이상용 <lsy3709@naver.com>
2024-12-03 14:25:59 +0900

    5교시, 예외처리, 404 not found 예외 확인하기

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
@@ -1,9 +1,12 @@
 package com.busanit501.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.util.Arrays;
 
@@ -11,6 +14,7 @@ import java.util.Arrays;
 @ControllerAdvice
 public class CommonExceptionAdvice {
 
+    // 우리가 봐야 할 에러 메세지.
     // @Controller : 1)화면 제공 2)데이터 제공
     // @RestController (@ResponseBody 내포가 되어 있음)
     // :  2)데이터 제공만
@@ -25,19 +29,27 @@ public class CommonExceptionAdvice {
         // 서버 -> 웹브라우저, 데이터만 전달 할 예정.
         // 리턴 타입 : String , 문자열로 데이터만 전달함.
         StringBuffer buffer = new StringBuffer("<ul>");
-        buffer.append("<li>"+exception.getMessage()+"</li>");
+        buffer.append("<li>" + exception.getMessage() + "</li>");
 
         // 예외가 발생하는 과정을 추적하는 코드도 같이 추가.
         // Stream 으로 , 여러 단계를 표현하기.
         // exception.getStackTrace() : 목록.
         //
         Arrays.stream(exception.getStackTrace())
-                .forEach(t -> buffer.append("<li>"+t+"</li>"));
+                .forEach(t -> buffer.append("<li>" + t + "</li>"));
 
         buffer.append("</ul>");
         return buffer.toString();
 
 
+    } //
+
+    //404 페이지 처리
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String notFound() {
+        return "custom404page";
     }
+
 
 }
