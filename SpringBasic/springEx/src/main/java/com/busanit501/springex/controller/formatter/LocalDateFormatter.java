15db82623f6c160bae71cc22153d08c63b017aba
이상용 <lsy3709@naver.com>
2024-12-03 10:25:37 +0900

    2교시, 데이터 자동 수집 및 형변환, 테스트3, LocalDate 받을 경우 servlet-context.xml 빈으로 등록

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/formatter/LocalDateFormatter.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/formatter/LocalDateFormatter.java
@@ -8,6 +8,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 // LocalDate <-> String, 형변환 작업 기능.
+// 기능으로 사용, 유틸 역할, 시스템에 빈으로 등록함.
 public class LocalDateFormatter implements Formatter<LocalDate> {
     @Override
     public LocalDate parse(String text, Locale locale) throws ParseException {
