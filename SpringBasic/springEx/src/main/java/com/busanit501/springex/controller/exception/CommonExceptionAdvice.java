574f74158b23d0458a76c51d937f18bcce2f3003
이상용 <lsy3709@naver.com>
2024-12-03 14:03:50 +0900

    5교시, 예외처리, 타입 불일치 예시로 강제로 예외 발생시키기, 숫자 타입 불일치 예외 확인

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
@@ -2,8 +2,21 @@ package com.busanit501.springex.controller.exception;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Log4j2
 @ControllerAdvice
 public class CommonExceptionAdvice {
+
+    // @Controller : 1)화면 제공 2)데이터 제공
+    // @RestController (@ResponseBody 내포가 되어 있음)
+    // :  2)데이터 제공만
+    @ResponseBody //서버에서 데이터만 전달하는 애너테이션.
+    @ExceptionHandler(NumberFormatException.class)
+    public String exceptNumber(NumberFormatException numberFormatException) {
+        return "numberFormatException:" + numberFormatException.getMessage();
+
+    }
+
 }
