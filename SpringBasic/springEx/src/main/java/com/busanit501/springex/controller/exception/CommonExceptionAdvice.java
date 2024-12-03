2d8dc68f6a34261643306bbfb37fd9051147ea91
이상용 <lsy3709@naver.com>
2024-12-03 14:09:14 +0900

    5교시, 예외처리, 일반적인 예외를 처리하는 예시,

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/exception/CommonExceptionAdvice.java
@@ -13,9 +13,20 @@ public class CommonExceptionAdvice {
     // @RestController (@ResponseBody 내포가 되어 있음)
     // :  2)데이터 제공만
     @ResponseBody //서버에서 데이터만 전달하는 애너테이션.
-    @ExceptionHandler(NumberFormatException.class)
-    public String exceptNumber(NumberFormatException numberFormatException) {
-        return "numberFormatException:" + numberFormatException.getMessage();
+//    @ExceptionHandler(NumberFormatException.class)
+//    public String exceptNumber(NumberFormatException numberFormatException) {
+//        return "numberFormatException:" + numberFormatException.getMessage();
+    @ExceptionHandler(Exception.class)
+    public String exceptCommon(Exception exception) {
+        log.error("예외가 발생할 경우 동작하는 클래스 ");
+        log.error(exception.getMessage());
+        // 서버 -> 웹브라우저, 데이터만 전달 할 예정.
+        // 리턴 타입 : String , 문자열로 데이터만 전달함.
+        StringBuffer buffer = new StringBuffer("<ul>");
+        buffer.append("<li>"+exception.getMessage()+"</li>");
+        buffer.append("</ul>");
+        return buffer.toString();
+
 
     }
 
