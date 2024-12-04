d0ff77e4e8fb27caa19b14cdb4b82b6639ae35ce
이상용 <lsy3709@naver.com>
2024-12-04 09:50:06 +0900

    1교시, 유효성 체크 확인 2

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/controller/TodoController.java
@@ -43,6 +43,7 @@ public class TodoController {
 
         // 유효성 체크 -> 유효성 검증시, 통과 안된 원인이 있다면,
         if (bindingResult.hasErrors()) {
+            log.info("has errors : 유효성 에러가 발생함.");
             // 1회용으로, 웹 브라우저에서, errors , 키로 조회 가능함. -> 뷰 ${errors}
             redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
             return "redirect:/todo/register";
