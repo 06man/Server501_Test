8f027f9ab159693cb49af945c72a099145f08128
이상용 <lsy3709@naver.com>
2024-12-06 15:04:50 +0900

    6교시, 검색, 필터 준비물 준비, DTO에 설정.

--- SpringBasic/springEx/src/main/java/com/busanit501/springex/dto/PageRequestDTO.java
+++ SpringBasic/springEx/src/main/java/com/busanit501/springex/dto/PageRequestDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Positive;
+import java.time.LocalDate;
 
 @Data
 @Builder
@@ -32,6 +33,18 @@ public class PageRequestDTO {
 
     //목록 -> 상세보기 화면 이동시, 페이지 정보를 쿼리 스트링으로 전달하는 용도 멤버
     private String link;
+
+    // 검색 또는 필터 관련 조건
+    // 1, 검색어
+    private String keyword;
+    //2, 검색 유형 ,1) 제목,t 2) 작성자,w 3) 제목 + 작성자,tw
+    private String[] types;
+    //3, todo 완료 여부
+    private boolean finished;
+    //4, 기한 1
+    private LocalDate from;
+    //4, 기한 2
+    private LocalDate to;
 
     // 데이터를 얼마나 스킵 할지 정할 기능.
     public int getSkip() {
