4569493782b209d53816465fcef242293f1c0092
이상용 <lsy3709@naver.com>
2024-12-10 14:29:50 +0900

    5교시, jpa 기초3, 엔티티 클래스로 테이블 만들기2, 베이스 엔티티 적용 및, 상속 및 멤버 제약조건 설정

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/domain/Board.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/domain/Board.java
@@ -1,19 +1,32 @@
 package com.busanit501.boot501.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity // JPA 이용해서, 엔티티 클래스 , 데이터베이스 테이블 만들기 놀이.
-public class Board {
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString //로그를 해당 인스턴스 찍을 때, 해당 멤버를 쉽게 확인 가능.
+public class Board extends BaseEntity { // 전역으로 만든, 베이스 엔티티 클래스  적용.
+
+    //2 번째 작업, 제약조건 넣기, 각 멤버는 각 디비의 컬럼과 동일함,
+    // 그래서,각각에 제약조건 설정하기.
+
     @Id // PK, 기본키,
     // Oracle, 시퀸스 객체, 이용시, SEQUENCE 설정, 추가 설정. 필요함.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 마리아디비,
     private Long bno;
 
+    //제약조건 넣기
+    @Column(length = 500, nullable = false)// 길이 500자,  NotNull=nn
     private String title;
+
+    @Column(length = 2000, nullable = false)// 길이 500자,  NotNull=nn
     private String content;
+
+    @Column(length = 50, nullable = false)// 길이 500자,  NotNull=nn
     private String writer;
     // 모든 테이블에 공통으로 들어갈수 있는, 등록시간, 수정시간, 등,
     // 베이스 엔티티에서 작업 할 예정.
