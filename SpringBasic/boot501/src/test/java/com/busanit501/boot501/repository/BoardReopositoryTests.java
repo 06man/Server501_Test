6c7ffd46b60ed5c2c4df69d03406958f8c7478c5
이상용 <lsy3709@naver.com>
2024-12-10 15:56:24 +0900

    7교시, jpa 기초3, 레포지토리 이용해서, 간단한 디비 작업 해보기 1, 부트에서 간단히 페이징 구현해보기4

--- SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
+++ SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
@@ -103,6 +103,8 @@ public class BoardReopositoryTests {
         log.info("result.getTotalElements()전체 갯수 :" +result.getTotalElements());
         log.info("result.getTotalPages()총페이지등 :" +result.getTotalPages());
         log.info("result.getContent() 페이징된 결과물 10개 :" +result.getContent());
+        log.info("result.getNumber() 현재 페이지 번호 :" +result.getNumber());
+        log.info("result.getSize() 크기  :" +result.getSize());
     }
 
 
