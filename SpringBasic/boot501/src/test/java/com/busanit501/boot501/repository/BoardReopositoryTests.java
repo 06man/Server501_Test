606c1be0a0f656afa295b948c5c4ea4419d15871
이상용 <lsy3709@naver.com>
2024-12-11 12:42:28 +0900

    4교시, Querydsl 이용하기, 전에, Querydsl 단위 테스트, 실제 검색 조건 이용해서 Page 타입으로  반환 받기2,

--- SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
+++ SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
@@ -139,6 +139,7 @@ public class BoardReopositoryTests {
 
     // 방버3 Querydsl
     // 단계적으로, sql 문장만 일단 확인중. , 아직 메서드 완성 안됨.
+    // 연습용
     @Test
     public void testQuerydsl() {
         Pageable pageable = PageRequest.of(1, 10,
@@ -151,6 +152,26 @@ public class BoardReopositoryTests {
 //        log.info("result.getContent() 페이징된 결과물 10개 :" +result.getContent());
 //        log.info("result.getNumber() 현재 페이지 번호 :" +result.getNumber());
 //        log.info("result.getSize() 크기  :" +result.getSize());
+    }
+
+    @Test
+    public void testQuerydsl2() {
+        Pageable pageable = PageRequest.of(1, 10,
+                Sort.by("bno").descending());
+
+        // 전달할 준비물
+        // 1) 검색어, 2) 검색 유형
+        String keyword = "3";
+        String[] types = {"t","w","c"};
+
+        Page<Board> result = boardRepository.searchAll(types,keyword,pageable);
+
+
+        log.info("result.getTotalElements()전체 갯수 :" +result.getTotalElements());
+        log.info("result.getTotalPages()총페이지등 :" +result.getTotalPages());
+        log.info("result.getContent() 페이징된 결과물 10개 :" +result.getContent());
+        log.info("result.getNumber() 현재 페이지 번호 :" +result.getNumber());
+        log.info("result.getSize() 크기  :" +result.getSize());
     }
 
 
