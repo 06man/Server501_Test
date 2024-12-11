76be726c9f2dab1e7c168af9158c92f430065617
이상용 <lsy3709@naver.com>
2024-12-11 10:45:44 +0900

    2교시, Querydsl 이용하기, BoardSearch , Repository, 우리가 만든 Querydsl 용 인터페이스 적용(추가)

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearch.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearch.java
@@ -4,6 +4,7 @@ import com.busanit501.boot501.domain.Board;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+// Querydsl 이용시, 해당 인터페이스에서, Jq
 public interface BoardSearch {
     Page<Board> search(Pageable pageable);
 }
