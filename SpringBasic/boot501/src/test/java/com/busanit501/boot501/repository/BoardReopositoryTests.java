01d981b8c62691db4ea255bf92f158f66bb4685c
이상용 <lsy3709@naver.com>
2024-12-11 12:45:25 +0900

    4교시, Querydsl 이용하기, 전에, Querydsl 단위 테스트, 실제 검색 조건 이용해서 Page 타입으로  반환 받기3

--- SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
+++ SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/BoardReopositoryTests.java
@@ -166,12 +166,13 @@ public class BoardReopositoryTests {
 
         Page<Board> result = boardRepository.searchAll(types,keyword,pageable);
 
-
         log.info("result.getTotalElements()전체 갯수 :" +result.getTotalElements());
         log.info("result.getTotalPages()총페이지등 :" +result.getTotalPages());
         log.info("result.getContent() 페이징된 결과물 10개 :" +result.getContent());
         log.info("result.getNumber() 현재 페이지 번호 :" +result.getNumber());
         log.info("result.getSize() 크기  :" +result.getSize());
+        log.info("result.hasNext() 다음  :" +result.hasNext());
+        log.info("result.hasPrevious() 이전  :" +result.hasPrevious());
     }
 
 
