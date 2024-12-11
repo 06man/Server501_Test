fc0845ba8f4bc0f4227bd1991d2355771b592053
이상용 <lsy3709@naver.com>
2024-12-11 12:22:31 +0900

    4교시, Querydsl 이용하기, 전에, Querydsl 단위 테스트, where 조건 추가하기. 하나 또는 여러개 형식

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearchImpl.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/repository/search/BoardSearchImpl.java
@@ -2,6 +2,7 @@ package com.busanit501.boot501.repository.search;
 
 import com.busanit501.boot501.domain.Board;
 import com.busanit501.boot501.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -23,6 +24,10 @@ implements BoardSearch {
     }
 
     @Override
+    // 자바 문법으로 , sql 문장 명령어 대체해서 전달중.
+    // 1) where
+    // 2) 페이징
+    // 3) 제목, 내용, 조건절. 추가중.
     public Page<Board> search(Pageable pageable) {
         //예시,
         QBoard board = QBoard.board; // Q 도메인 객체, 엔티티 클래스(= 테이블)
@@ -32,9 +37,22 @@ implements BoardSearch {
         // 예, where, groupby, join , pagination
         query.where(board.title.contains("3"));
         // =================================.,조건1
+
+        // 제목, 작성자 검색 조건 추가,
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        booleanBuilder.or(board.title.contains("3"));// "3" 제목 임시
+        booleanBuilder.or(board.content.contains("7"));// "3" 제목 임시
+        // query, 해당 조건을 적용함.
+        query.where(booleanBuilder);
+        // 방법2, 추가 조건으로, bno 가 0보다 초과 하는 조건.
+        query.where(board.bno.gt(0L));
+
+        // =================================.,조건3
+
         // 페이징 조건 추가하기. qeury에 페이징 조건을 추가한 상황
         this.getQuerydsl().applyPagination(pageable, query);
         // =================================.,조건2
+
         // 해당 조건의 데이터를 가져오기,
         List<Board> list = query.fetch();
         // 해당 조건에 맞는 데이터의 갯수 조회.
