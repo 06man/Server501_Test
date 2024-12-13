9ef40afba3a0eca36e2187d11c3e4351d6b09581
이상용 <lsy3709@naver.com>
2024-12-13 15:30:04 +0900

    6교시,  화면, 부트스트랩 템플릿 DashBoard, 간단 헤더 부트스트랩 이용하기 변경2

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/BoardController.java
@@ -1,6 +1,8 @@
 package com.busanit501.boot501.controller;
 
+import com.busanit501.boot501.dto.BoardDTO;
 import com.busanit501.boot501.dto.PageRequestDTO;
+import com.busanit501.boot501.dto.PageResponseDTO;
 import com.busanit501.boot501.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -19,6 +21,8 @@ public class BoardController {
     // http://localhost:8080/board/list
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model ) {
-        // 타임리프 작업
+        // 서비스 이용해서, 데이터베이스 목록 페이징 처리해서 가져오기.
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        model.addAttribute("responseDTO", responseDTO);
     }
 }
