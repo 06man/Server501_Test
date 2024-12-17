817dd7fffbafb72281d3da3619d1f233a073aae1
이상용 <lsy3709@naver.com>
2024-12-17 14:16:18 +0900

    5교시, RESTful 기초, 댓글 작업 준비5, @Valid 이용해서, 유효성 체크

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/ReplyController.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/ReplyController.java
@@ -2,10 +2,13 @@ package com.busanit501.boot501.controller;
 
 import com.busanit501.boot501.dto.ReplyDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,10 +43,16 @@ public class ReplyController {
     // 원래의 일반 클래스로 역직렬화,
     // Jackson 컨버터 도구가, 알아서, 역직렬화 해줌.
     public ResponseEntity<Map<String,Long>> register(
-            @RequestBody ReplyDTO replyDTO
-    ){
+            @Valid @RequestBody ReplyDTO replyDTO,
+            BindingResult bindingResult
+    ) throws BindException {
         log.info(" ReplyController replyDTO: ", replyDTO);
         // 확인용, 더미 데이터 ,
+
+        if (bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
         Map<String,Long> map = Map.of("rno",100L);
         return ResponseEntity.ok(map);
     }
--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/dto/ReplyDTO.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/dto/ReplyDTO.java
@@ -1,5 +1,7 @@
 package com.busanit501.boot501.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,9 +14,16 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class ReplyDTO {
-    private Long rno;
-    private Long bno;
+    private Long rno;// 댓글의 구분 번호,
+
+    @NotNull
+    private Long bno; // 부모의 게시글 번호,
+
+    @NotEmpty
     private String replyText;
+
+    @NotEmpty
     private String replyer;
+
     private LocalDateTime regDate,modDate;
 }
