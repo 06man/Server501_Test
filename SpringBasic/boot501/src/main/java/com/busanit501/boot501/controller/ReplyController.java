8adf2b5195286db31d45bb3ea4e54122c1bfcde8
이상용 <lsy3709@naver.com>
2024-12-17 11:07:50 +0900

    3교시, RESTful 기초, 댓글 작업 준비3, ReplyController, 포스트 맵핑 샘플링 작업

---
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/ReplyController.java
@@ -0,0 +1,33 @@
+package com.busanit501.boot501.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/replies")
+@Log4j2
+@RequiredArgsConstructor
+public class ReplyController {
+
+    //ReplyService 만들어서, 작업해야함.
+    @Tag(name = "댓글 등록 post 방식",
+            description = "댓글 등록을 진행함, post 형식으로")
+    @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String,Long> register(){
+
+        return null;
+    }
+
+}
+
+
+
+
+
