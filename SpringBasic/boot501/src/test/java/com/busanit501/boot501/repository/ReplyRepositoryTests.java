e6d34e1a47e73af1d2f0c2a5951968e4f8f6fbf0
이상용 <lsy3709@naver.com>
2024-12-18 15:37:43 +0900

    6교시, 댓글 화면 작업, read.html , 댓글 버튼, 댓글 목록, 댓글 페이징 번호 화면 영역 뼈대 잡기, 댓글의 전체 목록 조회 기능 빠져서 추가중. Repository 단위 테스트

--- SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/ReplyRepositoryTests.java
+++ SpringBasic/boot501/src/test/java/com/busanit501/boot501/repository/ReplyRepositoryTests.java
@@ -74,6 +74,23 @@ public class ReplyRepositoryTests {
         log.info("result.hasPrevious() 이전  :" +result.hasPrevious());
     }
 
+    @Test
+    public void testSelectWithReply() {
+        Pageable pageable = PageRequest.of(0, 10,
+                Sort.by("bno").descending());
+
+        Page<Reply> result = replyRepository.listOfBoard(122L,pageable);
+        log.info("result.getTotalElements()전체 갯수 :" +result.getTotalElements());
+        log.info("result.getTotalPages()총페이지등 :" +result.getTotalPages());
+        log.info("result.getContent() 페이징된 결과물 10개 :" +result.getContent());
+        log.info("result.getNumber() 현재 페이지 번호 :" +result.getNumber());
+        log.info("result.getSize() 크기  :" +result.getSize());
+        log.info("result.hasNext() 다음  :" +result.hasNext());
+        log.info("result.hasPrevious() 이전  :" +result.hasPrevious());
+    }
+
+
+
 }
 
 
