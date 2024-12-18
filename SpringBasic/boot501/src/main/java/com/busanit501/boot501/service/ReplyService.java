7cb1c1b22e7246ca5d77ec96e708ef3b94b38b65
이상용 <lsy3709@naver.com>
2024-12-18 15:28:07 +0900

    6교시, 댓글 화면 작업, read.html , 댓글 버튼, 댓글 목록, 댓글 페이징 번호 화면 영역 뼈대 잡기, 댓글의 전체 목록 조회 기능 빠져서 추가중.

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/service/ReplyService.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/service/ReplyService.java
@@ -1,5 +1,7 @@
 package com.busanit501.boot501.service;
 
+import com.busanit501.boot501.dto.PageRequestDTO;
+import com.busanit501.boot501.dto.PageResponseDTO;
 import com.busanit501.boot501.dto.ReplyDTO;
 
 public interface ReplyService {
@@ -7,5 +9,8 @@ public interface ReplyService {
     ReplyDTO readOne(Long rno);
     void update(ReplyDTO replyDTO);
     void delete(Long rno);
-//    PageResponseDTO<BoardDTO> list(PageRequestDTO pageRequestDTO);
+    // 부모 게시글 번호에 대한 댓글 목록 조회.
+    PageResponseDTO<ReplyDTO> listWithReply(Long bno,PageRequestDTO pageRequestDTO);
 }
+
+
