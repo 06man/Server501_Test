b8dfc24f6465df4a31d9f71df1423f748e3711d7
이상용 <lsy3709@naver.com>
2024-12-20 12:10:49 +0900

    4교시, 파일업로드 기초1, 준비3, 업로드 저장소 위치를 컨트롤러에서 사용하기

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/UpdownController.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/controller/UpdownController.java
@@ -3,6 +3,7 @@ package com.busanit501.boot501.controller;
 import com.busanit501.boot501.dto.upload.UploadFileDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Log4j2
 public class UpdownController {
+    // application.propeties, 업로드 경로 설정.
+    // com.busanit501.upload.path=c:\\upload\\springTest
+    // 컨트롤러에서 사용해보기.
+    @Value("${com.busanit501.upload.path}")
+    private String uploadPath;
 
     @Tag(name = "파일 등록 post",
             description = "멀티파트 타입 형식 이용해서, post 형식으로 업로드테스트")
