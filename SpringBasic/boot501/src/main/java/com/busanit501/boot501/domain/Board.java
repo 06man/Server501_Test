c7f68a5669a7ff28bfe804f5a2dd64d3df4bf277
이상용 <lsy3709@naver.com>
2024-12-24 10:07:15 +0900

    2교시, OneToMany, 양방향 설정함, 중간테이블 생성하는 부분확인

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/domain/Board.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/domain/Board.java
@@ -3,12 +3,15 @@ package com.busanit501.boot501.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity // JPA 이용해서, 엔티티 클래스 , 데이터베이스 테이블 만들기 놀이.
 @Getter // 비지니스 모델, 디비에는 불변성 유지, 수정안함., 안에 메서드 이용해서 멤버 교체식으로함.
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString //로그를 해당 인스턴스 찍을 때, 해당 멤버를 쉽게 확인 가능.
+@ToString(exclude ="imageSet")//로그를 해당 인스턴스 찍을 때, 해당 멤버를 쉽게 확인 가능.
 public class Board extends BaseEntity { // 전역으로 만든, 베이스 엔티티 클래스  적용.
 
     //2 번째 작업, 제약조건 넣기, 각 멤버는 각 디비의 컬럼과 동일함,
@@ -28,6 +31,13 @@ public class Board extends BaseEntity { // 전역으로 만든, 베이스 엔티
 
     @Column(length = 50, nullable = false)// 길이 500자,  NotNull=nn
     private String writer;
+
+    // 연관관계 설정,
+    @OneToMany
+    @Builder.Default
+    private Set<BoardImage> imageSet = new HashSet<>();
+
+
     // 모든 테이블에 공통으로 들어갈수 있는, 등록시간, 수정시간, 등,
     // 베이스 엔티티에서 작업 할 예정.
 
