2b4759b7caa39ec419c63fe7f002a8e00e2b0ad4
이상용 <lsy3709@naver.com>
2024-12-30 10:45:30 +0900

    2교시, 시큐리티 기초2, 설정 클래스 확인7, 로그인 페이지 화면 확인. 설정 클래스 로그인 페이지 경로 수정

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
@@ -25,7 +25,7 @@ public class CustomSecurityConfig {
         // http.formLogin();
         http.formLogin(
                 formLogin ->
-                        formLogin.loginPage("/login")
+                        formLogin.loginPage("/member/login")
         );
 
 
