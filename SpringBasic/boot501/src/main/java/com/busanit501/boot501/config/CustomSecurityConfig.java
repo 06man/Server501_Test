0a8975ff53d0e7d9146ea73ae57cee9ef9871f12
이상용 <lsy3709@naver.com>
2024-12-30 10:12:27 +0900

    2교시, 시큐리티 기초2, 설정 클래스 확인4 정적 자원 시큐리티 필터 제외

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/config/CustomSecurityConfig.java
@@ -2,9 +2,11 @@ package com.busanit501.boot501.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Log4j2
@@ -12,11 +14,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
 
+    // 인증, 인가 관련 구체적인 설정은 여기 메서드에서 작성
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         log.info("===========config=================");
 
         return http.build();
+    }
+
+    // css, js, 등 정적 자원은 시큐리티 필터에서 제외하기
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        log.info("시큐리티 동작 확인 ====webSecurityCustomizer======================");
+        return (web) ->
+                web.ignoring()
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
 
