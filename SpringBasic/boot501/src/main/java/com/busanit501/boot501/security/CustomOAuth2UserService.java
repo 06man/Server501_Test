d8c5237cb160b9eb971ba6629f33deffb9308066
이상용 <lsy3709@naver.com>
2025-01-03 11:23:54 +0900

    3교시, 카카오로그인 연동, 소셜 로그인 정보 중에서, 먼저 이메일 먼저 출력해보기

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
@@ -9,6 +9,7 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 @Log4j2
@@ -30,11 +31,34 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
         Map<String, Object> paramMap = oAuth2User.getAttributes();
+        //paramMap
+        //키: properties, 값 : 객체 안에(맵구조), nickname=이상용
+        //키: properties, 값 : 객체 안에(맵구조), profile_image=http://k.kakaocdn.net/dn/IbxaJ/btsJ3hcSYmJ/6zalcIKKnKf3gJNqEusBo0/img_640x640.jpg
+        //키: kakao_account, 값 : 객체 안에(맵구조), email=lsy3709@kakao.com
 
+        // 소셜 정보 출력 확인용
         paramMap.forEach((key, value) -> {
             log.info("key : " + key + " value : " + value);
         });
 
+        String email = null;
+
+        switch (clientName) {
+            case "kakao":
+                //getKakaoEmail(paramMap) , 메서드를 밑에 정의
+                email = getKakaoEmail(paramMap);
+                break;
+        }
+
         return oAuth2User;
     }
+
+    private String getKakaoEmail(Map<String, Object> params) {
+        Object value = params.get("kakao_account");
+        LinkedHashMap accountMap = (LinkedHashMap) value;
+        String email = (String) accountMap.get("email");
+        log.info("email : " + email);
+        return email;
+    }
+
 }
