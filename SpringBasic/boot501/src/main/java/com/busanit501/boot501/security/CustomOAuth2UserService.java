888f1443ca0b5496f283071b5dffb39c4c63f64c
이상용 <lsy3709@naver.com>
2025-01-03 10:41:28 +0900

    2교시, 카카오로그인 연동, OAuth2 로그인 형식에 맞는 로그인 구현 메서드 작업중2, 이메일 추출 준비, 전달받은 소셜 정보 조회

--- SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
+++ SpringBasic/boot501/src/main/java/com/busanit501/boot501/security/CustomOAuth2UserService.java
@@ -2,11 +2,14 @@ package com.busanit501.boot501.security;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Log4j2
 @Service
@@ -18,6 +21,20 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
         log.info("kakao login CustomOAuth2UserService userRequest");
         log.info(userRequest);
 
-        return super.loadUser(userRequest);
+        // userRequest: 소셜 유저의 정보 담겨 있음.
+        // 이메일 추출 해보기.
+        ClientRegistration clientRegistration
+                = userRequest.getClientRegistration();
+        String clientName = clientRegistration.getClientName();
+        log.info("clientName : " + clientName);
+
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        Map<String, Object> paramMap = oAuth2User.getAttributes();
+
+        paramMap.forEach((key, value) -> {
+            log.info("key : " + key + " value : " + value);
+        });
+
+        return oAuth2User;
     }
 }
